e85afb8dde44b9a96e446712e1c1191a304b9381
yefanflexi <yefan_flexi@163.com>
2018-11-09 13:30:39 +0800

    基本功能实现2

---
+++ ay-role-service/src/main/java/com/example/ayroleservice/AyRoleServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.ayroleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyRoleServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AyRoleServiceApplication.class, args);
+    }
+}
